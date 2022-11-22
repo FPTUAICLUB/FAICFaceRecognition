3308a2310b030926f48648a55d6b23ec725a9e13
dungmaster <dungnguyen2712002@gmail.com>
2022-11-22 23:04:56 +0700

    update saving face

--- save_face.py
+++ save_face.py
@@ -10,24 +10,30 @@ from icecream import ic
 from utils.audio import *
 import os
 import time
+from main import preprocess
 
 def get_args():
     parser = argparse.ArgumentParser(description='Face Recognition')
-    parser.add_argument('-e', '--enlarge', type=int, default=20)
-    parser.add_argument('-n', '--name', type=str, default='')
-    parser.add_argument('-r', '--restart', action='store_true')
+    parser.add_argument('-e', '--enlarge', help='Enlarging face bboxes in 4 directions', type=int, default=20)
+    parser.add_argument('-n', '--name', help='Name of saved face',type=str, default='')
+    parser.add_argument('-r', '--restart', help='Restart saving faces from zero', action='store_true')
+    parser.add_argument('-s', '--save-embeddings', help='Save face embedding', action='store_true')
     args = parser.parse_args()
     return args    
 
 class Detector:
-    def __init__(self, enlarge, name, restart, threshold=0.5, use_cuda=True):
-        # self.known_face_embs = np.squeeze(load_pickle('embedding_data/embed_faces.pkl'), axis=1)
-        # self.known_names = load_pickle('embedding_data/labels.pkl')
+    def __init__(self, enlarge, name, restart, save_embeddings=False, threshold=0.5, use_cuda=True):
+
         self.name = name
         self.thr = threshold
         self.enlarge = enlarge
         self.restart = restart
-        # self.ort_sess = ort.InferenceSession('checkpoints/webface_r50.onnx', providers=['CUDAExecutionProvider'])
+        self.save_embeddings = save_embeddings
+        if save_embeddings:
+            self.known_face_embs = np.squeeze(load_pickle('embedding_data/embed_faces.pkl'), axis=1)
+            self.known_names = load_pickle('embedding_data/labels.pkl')
+            self.ort_sess = ort.InferenceSession('checkpoints/webface_r50.onnx', providers=['CUDAExecutionProvider'])
+            ic(self.known_face_embs.shape[0])
 
         # load model
         self.faceModel = cv2.dnn.readNetFromCaffe('checkpoints/res10_300x300_ssd_iter_140000.prototxt',
@@ -81,10 +87,18 @@ class Detector:
                 for bbox in bboxes:
                     face = self.img[bbox[1]:bbox[3],
                                     bbox[0]:bbox[2]]
+                    
+                    if self.save_embeddings and face.shape[0]*face.shape[1] != 0:
+                        face_pr = preprocess(face)
+                        input_name = self.ort_sess.get_inputs()[0].name
+                        emb = self.ort_sess.run([], {input_name: face_pr})[0]
 
                     cv2.rectangle(self.img, bbox[:2], bbox[2:4], (255, 0, 255), 2)
 
-                if t2 - t1 >= 1:
+                if face.shape[0]*face.shape[1] != 0 and t2 - t1 >= 1:
+                    ic(count)
+                    self.known_face_embs = np.append(self.known_face_embs, emb, axis=0)
+                    self.known_names.append(self.name)
                     cv2.imwrite(osp.join(new_face_dir, str(count)+'.jpg'), face)
                     count += 1
                     t1 = t2
@@ -97,6 +111,7 @@ class Detector:
 
                 ret, self.img = cap.read()
 
+            ic(self.known_face_embs.shape[0])
             cap.release()
             cv2.destroyAllWindows()
 
@@ -106,5 +121,6 @@ if __name__ == '__main__':
     enlarge = cfg.enlarge
     save_name = cfg.name
     restart = cfg.restart
-    det = Detector(enlarge, save_name, restart)
+    save_embeddings = cfg.save_embeddings
+    det = Detector(enlarge, save_name, restart, save_embeddings)
     det.checkInVideo(0)
